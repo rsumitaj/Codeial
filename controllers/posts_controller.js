a177e3bbeb2edb1c6231a23995621cb438df110e
rsumitaj <sumitrajtech@gmail.com>
2021-06-02 17:02:46 +0530

    setup view engine for home

--- controllers/posts_controller.js
+++
@@ -1,3 +0,0 @@
-module.exports.posts = function(req,res){
-  return res.end('<h1>User Posts</h1>');
-}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,9 @@
 module.exports.profile = function(req,res){
-  return res.end('<h1>User Profile</h1>');
+  return res.render('profile',{
+    title:"Profile"
+  });
+}
+
+module.exports.posts = function(req,res){
+  return res.end('<h1>User Posts</h1>');
 }
