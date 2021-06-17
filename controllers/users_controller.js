05c1043c21381df6d63b8695613a354ebd8a3331
rsumitaj <sumitrajtech@gmail.com>
2021-06-17 18:15:12 +0530

    setup passport js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-  return res.render('profile',{
+  return res.render('user_profile',{
     title:"Profile"
   });
 }
