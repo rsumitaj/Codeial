4c09712a768bbb3b042151c0fdd2e70a54e8fef4
rsumitaj <sumitrajtech@gmail.com>
2021-07-09 09:53:29 +0530

    delete a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/posts");
+const Comment = require('../models/comment');
 
 
 module.exports.create = function(req,res){
@@ -11,5 +12,21 @@ module.exports.create = function(req,res){
       return;
     }
     return res.redirect('back');
+  });
+}
+
+module.exports.destroy = function(req, res){
+  Post.findById(req.params.id, function(err, post){
+      // .id means converting the object id into string
+      if (post.user == req.user.id){
+          post.remove();
+
+          Comment.deleteMany({post: req.params.id}, function(err){
+              return res.redirect('back');
+          });
+      }else{
+          return res.redirect('back');
+      }
+
   });
 }
