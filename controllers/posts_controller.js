573444d7fc5b3668a188e97275a20cfa04082706
rsumitaj <sumitrajtech@gmail.com>
2021-07-15 20:21:50 +0530

    convertt to AJAX

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -5,10 +5,19 @@ const Comment = require('../models/comment');
 module.exports.create = async function(req,res){
   try{
 
-    await Post.create({
+    let post = await Post.create({
       content:req.body.content,
       user:req.user._id
     });
+
+    if(req.xhr){
+      return res.status(200).json({
+        data:{
+          post:post
+        },
+        message:'Post created !!'
+      });
+    }
 
     req.flash('success',"Post Created !!");
     return res.redirect('back');
@@ -29,6 +38,15 @@ module.exports.destroy = async function(req, res){
       post.remove();
   
       await Comment.deleteMany({post: req.params.id});
+
+      if(req.xhr){
+        return res.status(200).json({
+          data:{
+            post_id:req.params.id
+          },
+          message:"Post deleted"
+        });
+      }
 
       req.flash('success','Post and comments deleted');
       return res.redirect('back');
