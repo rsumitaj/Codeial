86cbab067333816f610c87eb1781d1ea6c9c5a7a
rsumitaj <sumitrajtech@gmail.com>
2021-07-17 01:21:02 +0530

    replace avatar on updating

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = function(req,res){
   User.findById(req.params.id,function(err,user){
@@ -33,6 +35,10 @@ module.exports.update = async  function(req,res){
         user.email = req.body.email;
 
         if(req.file){
+
+          if(user.avatar){
+            fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+          }
           // this is saving the path of the uploaded file into the avatar field in the user
           user.avatar = User.avatarPath + '/' + req.file.filename ;
         }
