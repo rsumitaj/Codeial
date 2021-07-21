9a342358f8394b643aca81b33c9f70a7290a1422
rsumitaj <sumitrajtech@gmail.com>
2021-07-21 13:12:41 +0530

    delete post from postman

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,49 @@
+const Post = require('../../../models/posts');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req,res){
+
+  let posts = await Post.find({})
+  .sort('-createdAt')
+  .populate('user')
+  .populate({
+    path:'comments',
+    populate:{
+      path:'user'
+    }
+  });
+
+  return res.json(200,{
+    message : "List of posts" ,
+    post_list: posts
+  })
+} ;
+
+module.exports.destroy = async function(req, res){
+
+  try{
+
+    let post = await Post.findById(req.params.id);
+
+    // if (post.user == req.user.id){
+      post.remove();
+  
+      await Comment.deleteMany({post: req.params.id});
+
+      return res.json(200,{
+        message : "Post and associated Comments deleted successfully"
+      });
+    // }else{
+    //   req.flash('error','You cannot delete this post');
+    //     return res.redirect('back');
+    // }
+
+  }catch(err){
+    // req.flash('error',err);
+    return res.json(500,{
+      message:"Internal Serveer Error"
+    });
+  }
+  
+}
+
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -23,7 +23,7 @@ module.exports.home = async function(req,res){
       path:'user'
     }
   });
-
+  
   let users = await User.find({});
 
   return res.render('home',{
